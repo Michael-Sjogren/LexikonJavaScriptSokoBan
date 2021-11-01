a93a57fe0ed52a89a57c0de68e2eb1eed762a5ee
michael <carl.michael.sjogren@gmail.com>
2021-11-01 17:03:59 +0100

    main script and player class

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import Vector2 from './classes/vector2.js';
+import * as SokobanData from './sokoban-data.js';
+// the size of each tile; width and height.
+const tileSize = new Vector2(64 ,64);
+
+createMap = () => {
+    
+}
+
+
+console.log(tileSize);
+console.log("test");
+
