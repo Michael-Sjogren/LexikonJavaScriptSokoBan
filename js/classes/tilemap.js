8fdbe9a080986986bef9c40cd8accbc763359c07
michael <carl.michael.sjogren@gmail.com>
2021-11-02 16:15:16 +0100

    simplified main tilemap now creates and renders itself.

--- js/classes/tilemap.js
+++ js/classes/tilemap.js
@@ -1,16 +1,94 @@
-import Vector2 from './vector2.js';
+import Block from './block.js';
 import Tile from './tile.js';
 import { TileTypes } from '../sokoban-data.js';
+import Player from './player.js';
+import Vector2 from './vector2.js';
+
 export default class TileMap
 {
     
-    constructor(tileSize,mapSize)
+    constructor(mapGrid , tileSize ,gameContainer)
     {
-        this.tileSize = tileSize
-        this.mapSize = mapSize
-        this.tiles = {}
+        this.gameContainer = gameContainer;
+        this.tileSize = tileSize;
+        this.mapGrid = mapGrid;
+
+        this.mapSize = new Vector2(
+            this.mapGrid[0].length,
+            this.mapGrid.length
+        );
+        gameContainer.style = `min-width:${this.mapSize.x * this.tileSize.x}px;min-height:${this.mapSize.y* this.tileSize.y}px;`;
+        this.tiles = {};
+        this.goals = [];
+        this.blocks = [];
+        this.player = null;
+        this.createMap();
     }
 
+    getGoals = () => {
+        return this.goals;
+    }
+
+    getBlocks = () => {
+        return this.blocks;
+    }
+
+    getPlayer = () => {
+        return this.player;
+    }
+
+    createMap = () => {
+        const rows = this.mapSize.y;
+        const columns = this.mapSize.x;
+        // loop over the whole map
+        for(let row = 0; row < rows; row++)
+        {
+            let rowData = this.mapGrid[row];
+            let rowContainer = document.createElement('div');
+            rowContainer.className = "row";
+            rowContainer.style = `height:${this.tileSize.y}px;`;
+            for(let col = 0; col < columns; col++)
+            {
+                // add the tile to the tilemap
+                const tilePos = new Vector2(col * this.tileSize.x , row * this.tileSize.y);
+                const tileType = rowData[col][0];
+                const cellId = this.addTile(tilePos ,tileType);
+                if (tileType === TileTypes.block)
+                {
+                    this.blocks.push(new Block(tilePos,cellId));
+                }
+                else if(tileType === TileTypes.goal)
+                {
+                    this.goals.push(cellId);
+                }
+                else if(tileType === TileTypes.player)
+                {
+                    this.player = new Player(tilePos , cellId);
+                }
+                // creating the html elements and setting classes and id attributes
+                let tile = document.createElement('div');
+                tile.className = `tile ${TileTypes[tileType]}`;
+                tile.style = `width:${this.tileSize.x}px; height:${this.tileSize.y}px;`;
+                tile.id = cellId;
+                rowContainer.appendChild(tile);
+            }
+            this.gameContainer.appendChild(rowContainer);
+        }
+    };
+
+    renderMap = () => {
+     
+        for(let row = 0; row < this.mapSize.y; row++){
+            for(let col = 0; col < this.mapSize.x; col++)
+            {
+                const worldPos = new Vector2(col * this.tileSize.x , row * this.tileSize.y);
+                const cellId = this.getCellIdFromWorldPos(worldPos);
+                const tile = this.getTile(cellId);
+                const element = document.getElementById(cellId);
+                element.className = `tile ${TileTypes[tile.tileType]}`;
+            }
+        }
+    }
 
     getAllBlocks = () => {
         return []; 
@@ -60,7 +138,7 @@ export default class TileMap
 
     // just concatinates x and y position with a space between as an id
     getCellIdFromWorldPos = (worldPos) => {
-        return  `${worldPos.x} ${worldPos.y}`;
+        return  `x${worldPos.x}y${worldPos.y}`;
     }
 
     getTile = (cellId) => {
--- js/main.js
+++ js/main.js
@@ -1,86 +1,31 @@
-import Player from './classes/player.js';
+
 import TileMap from './classes/tilemap.js';
 import Vector2 from './classes/vector2.js';
-import Block from './classes/block.js';
+
 import * as SokobanData from './sokoban-data.js';
 
 // the size of each tile; width and height.
 const gameContainer = document.getElementById("game");
-const tileSize = new Vector2(32 ,32);
-const rows = SokobanData.TileMap01.mapGrid.length;
-const columns = SokobanData.TileMap01.mapGrid[0].length;
-const tileMap = new TileMap(tileSize,new Vector2(columns,rows));
-const blocks = [];
-const goals = [];
-var player = null
+const tileSize = new Vector2(32, 32);
+
+const tileMap = new TileMap(SokobanData.TileMap01.mapGrid, tileSize, gameContainer);
+
 const keyBindings = {
-    Up:['ArrowUp','w'],
-    Down:['ArrowDown','s'],
-    Right:['ArrowRight','d'],
-    Left:['ArrowLeft','a'],
-    Restart:['r']
+    Up: ['ArrowUp', 'w'],
+    Down: ['ArrowDown', 's'],
+    Right: ['ArrowRight', 'd'],
+    Left: ['ArrowLeft', 'a'],
+    Restart: ['r']
 }
 
-let createMap = () => {
-    for(let row = 0; row < rows; row++)
-    {
-        let rowData = SokobanData.TileMap01.mapGrid[row];
-        let rowContainer = document.createElement('div');
-        rowContainer.className = "row";
-        rowContainer.style = `height:${tileSize.y}px;`;
-        for(let col = 0; col < columns; col++)
-        {
-            // add the tile to the tilemap
-            let value = rowData[col];
-            let tilePos = new Vector2(col * tileSize.x , row * tileSize.y);
-            let tileType = value[0];
-            const cellId = tileMap.addTile(tilePos ,tileType);
-            if (tileType === SokobanData.TileTypes.block)
-            {
-                blocks.push(new Block(tilePos,cellId));
-            }
-            else if(tileType === SokobanData.TileTypes.goal)
-            {
-                goals.push(cellId);
-            }
-            else if(tileType === SokobanData.TileTypes.player)
-            {
-                player = new Player(tilePos , cellId);
-            }
-            // creating the html elements and setting classes and id attributes
-            let tile = document.createElement('div');
-            tile.className = `tile ${SokobanData.TileTypes[tileType]}`;
-            tile.style = `width:${tileSize.x}px; height:${tileSize.y}px;`;
-            tile.id = cellId;
-            rowContainer.appendChild(tile);
-        }
-        gameContainer.appendChild(rowContainer);
-    }
-};
-
-let renderMap = () => {
-     
-    for(let row = 0; row < rows; row++){
-        for(let col = 0; col < columns; col++)
-        {
-            const worldPos = new Vector2(col * tileSize.x , row * tileSize.y);
-            const cellId = tileMap.getCellIdFromWorldPos(worldPos);
-            const tile = tileMap.getTile(cellId);
-            const element = document.getElementById(cellId);
-            element.className = `tile ${SokobanData.TileTypes[tile.tileType]}`;
-        }
-    }
-}
-
-let resetGame = () =>
-{
+let resetGame = () => {
     window.location.reload();
 }
-let isGameWon = () => 
-{
-    return blocks.every(e => goals.includes(e.cellId));
+
+let isGameWon = () => {
+    return tileMap.getBlocks().every(e => tileMap.getGoals().includes(e.cellId));
 }
-createMap();
+
 
 
 document.addEventListener('keydown', (event) => {
@@ -88,76 +33,71 @@ document.addEventListener('keydown', (event) => {
     gameContainer.focus();
 
     const key = event.key;
-    let moveDir = new Vector2(0,0);
-    if (keyBindings.Restart.includes(key))
-    {
+    const player = tileMap.getPlayer();
+    const blocks = tileMap.getBlocks();
+    const goals = tileMap.getGoals();
+    const moveDir = new Vector2(0, 0);
+    if (keyBindings.Restart.includes(key)) {
         resetGame();
         return;
     }
-    if(keyBindings.Up.includes(key)){
+    if (keyBindings.Up.includes(key)) {
         moveDir.y = -1;
-    }else if(keyBindings.Down.includes(key))
-    {
+    } else if (keyBindings.Down.includes(key)) {
         moveDir.y = 1;
     }
-    else if(keyBindings.Right.includes(key))
-    {
+    else if (keyBindings.Right.includes(key)) {
         moveDir.x = 1;
     }
-    else if(keyBindings.Left.includes(key))
-    {
+    else if (keyBindings.Left.includes(key)) {
         moveDir.x = -1;
     }
-    else
-    {
+    else {
         return;
     }
-    
-    const newPlayerPos = new Vector2(moveDir.x * tileSize.x + player.position.x , moveDir.y * tileSize.y + player.position.y);
-    //console.log(`player: ${player.position.x} ${player.position.y} to: => ${moveWordPos.x} ${moveWordPos.y}`);
-    let playerCanMove = true;
-    if(tileMap.isTileWalkable(newPlayerPos))
-    {
-        if (tileMap.isTileBlock(newPlayerPos))
-        {
-            const cellId = tileMap.getCellIdFromWorldPos(newPlayerPos);
-            const block = blocks.find( e => e.cellId === cellId);
-            const newBlockPos = new Vector2( block.position.x + tileSize.x * moveDir.x , block.position.y + tileSize.y * moveDir.y )
-            if (!tileMap.isTileBlock(newBlockPos) && !tileMap.isTileWall(newBlockPos) )
-            {
-                
-                tileMap.setTileType(block.position , SokobanData.TileTypes.player);
-                block.moveTo(newBlockPos);
-                tileMap.setTileType(block.position , SokobanData.TileTypes.block );
-                block.cellId = tileMap.getCellIdFromWorldPos(block.position);
-            }
-            else
-            {
-                playerCanMove = false;
-            }
-        }
-        const prevId = tileMap.getCellIdFromWorldPos(player.position);
-        if (playerCanMove)
-        {
-            if (goals.includes(prevId)) {
-                tileMap.setTileType(player.position , SokobanData.TileTypes.goal);
-            }
-            else
-            {
-                tileMap.setTileType(player.position , SokobanData.TileTypes.empty);
-            }
-            player.moveTo(newPlayerPos);
-            const cellId = tileMap.getCellIdFromWorldPos(player.position);
-            tileMap.setTileType(player.position , SokobanData.TileTypes.player);
-            player.cellId = cellId;
-        }
 
-        renderMap();
-        if(isGameWon())
-        {
-            alert("Congratulations you win!");
-            resetGame();
+    const newPlayerPos = new Vector2(
+        moveDir.x * tileSize.x + player.position.x,
+        moveDir.y * tileSize.y + player.position.y
+    );
+
+    // check if player can move to desired position
+    if (!tileMap.isTileWalkable(newPlayerPos)) return;
+    let playerCanMove = true;
+    // check if the desired position for player is a movable block
+    if (tileMap.isTileBlock(newPlayerPos)) {
+        const cellId = tileMap.getCellIdFromWorldPos(newPlayerPos);
+        const block = blocks.find(e => e.cellId === cellId);
+        const newBlockPos = new Vector2(block.position.x + tileSize.x * moveDir.x, block.position.y + tileSize.y * moveDir.y)
+        if (!tileMap.isTileBlock(newBlockPos) && !tileMap.isTileWall(newBlockPos)) {
+
+            tileMap.setTileType(block.position, SokobanData.TileTypes.player);
+            block.moveTo(newBlockPos);
+            tileMap.setTileType(block.position, SokobanData.TileTypes.block);
+            block.cellId = tileMap.getCellIdFromWorldPos(block.position);
         }
+        else {
+            playerCanMove = false;
+        }
+    }
+    const prevId = tileMap.getCellIdFromWorldPos(player.position);
+    if (playerCanMove) {
+        if (goals.includes(prevId)) {
+            tileMap.setTileType(player.position, SokobanData.TileTypes.goal);
+        }
+        else {
+            tileMap.setTileType(player.position, SokobanData.TileTypes.empty);
+        }
+        player.moveTo(newPlayerPos);
+        const cellId = tileMap.getCellIdFromWorldPos(player.position);
+        tileMap.setTileType(player.position, SokobanData.TileTypes.player);
+        player.cellId = cellId;
+    }
+
+    tileMap.renderMap();
+    if (isGameWon()) {
+        alert("Congratulations you win!");
+        resetGame();
     }
 
 });
